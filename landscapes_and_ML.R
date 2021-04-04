04e00b1a66d6c01cdfa08545bd54263b00e1d1e6
Parker Edwards <parker.edwards@nd.edu>
2021-04-04 15:30:02 -0400

    Adds number of folds option.

--- landscapes_and_ML.R
+++ landscapes_and_ML.R
@@ -12,6 +12,7 @@ option_list <- list(optparse::make_option(c("--parameters"),type="character",hel
                     optparse::make_option(c("--upper"),type="numeric",default=1,help="Experimental. Maximum percentage distance from centroid for patches."),
                     optparse::make_option(c("--proportion"),type="numeric",default=.025,help="Experimental. Proportion of online pixels in each patch to sample."),
                     optparse::make_option(c("--pca"),type="character",default=FALSE,help="Experimental. Use PCA projection for summaries."),
+                    optparse::make_option(c("--folds"),type="numeric",default=5,help="Number of folds to use for cross-validaton. Default is 5."),
                     optparse::make_option(c("--fullsave"),type="character",default=FALSE,help="Debugging. If flag is set to TRUE then uses save.image at end. Default FALSE."),
                     optparse::make_option(c("--benchmark"),type="character",default=FALSE,help="Debugging. If TRUE, uses a fork based cluster type for benchmarking. Does not work on Windows."))
 opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
@@ -33,7 +34,7 @@ for(command_line_option in options_to_falsify) {
 }
 
 if(opt$landscapes!=FALSE) { 
-  training_results <- TDAExplore::TDAExplore(parameters=opt$parameters,number_of_cores=opt$cores,radius_of_patches=opt$radius,patch_ratio=opt$ratio,svm=opt$svm,lower=opt$lower,upper=opt$upper,proportion=opt$proportion,verbose=TRUE,benchmark=opt$benchmark,pca=opt$pca)  
+  training_results <- TDAExplore::TDAExplore(parameters=opt$parameters,number_of_cores=opt$cores,radius_of_patches=opt$radius,patch_ratio=opt$ratio,svm=opt$svm,lower=opt$lower,upper=opt$upper,proportion=opt$proportion,verbose=TRUE,benchmark=opt$benchmark,pca=opt$pca,folds=opt$folds)  
   save(training_results,version=2,file=file.path(training_results$data_results_directory,paste(training_results$data_name_stem,".RData",sep="")))
 }
 
